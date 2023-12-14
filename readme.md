6b26905fe9b45eaee312348d6894256bce75dcaf
shivansh <shivansh.m2003@gmail.com>
2023-12-14 19:59:01 +0530

    code files

--- readme.md
+++ readme.md
@@ -1,6 +1,1 @@
----
-noteId: "a01a17a09a8011ee979769ec76a90bd9"
-tags: []
-
----
-
+#machine Legarning Projects
--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
 from setuptools import find_packages,setup
 from typing import List
 
+HYPEN_E_DOT='-e .'
+
 def get_requirements(file_path:str)->List[str]:
     requirement=[]
     with open(file_path) as file_obj:
         file_obj.readlines()
         requirement=[req.replace("\n","")for req in requirement]
 
+        if HYPEN_E_DOT in requirement:
+            requirement.remove(HYPEN_E_DOT)
+
         return requirement
 
 setup(
     name='Diamond price prediction',
     version='0.0.1',
-    aurthor='ShivanshMahajan',
+    author='ShivanshMahajan',
     author_email='shivansh.m2003@gmail.com',
     install_requirement=get_requirements('requirement.txt'),
     packages=find_packages()
